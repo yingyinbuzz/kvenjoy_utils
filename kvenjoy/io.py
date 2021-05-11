734d2f282f63e0289c659dae3f138dfa5cef65dd
Ying YIN <ying.yin.buzz@outlook.com>
2021-05-11 16:01:21 +0800

    Add IO wrapper for reading/writing files.

---
+++ kvenjoy/io.py
@@ -0,0 +1,142 @@
+"""
+Stream input/output wrappers.
+
+Byte order is big endian.
+"""
+import struct
+
+def read_byte(stm, count = 1):
+    """Read byte(s) from stream.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of bytes to be read.
+    """
+    return _read(stm, '>{}B'.format(count))
+
+def read_short(stm, count = 1):
+    """Read half word(s) from stream.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of half words to be read.
+    """
+    return _read(stm, '>{}H'.format(count))
+
+def read_int(stm, count = 1):
+    """Read integer(s) from stream.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of integers to be read.
+    """
+    return _read(stm, '>{}I'.format(count))
+
+def read_float(stm, count = 1):
+    """Read float(s) from stream.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of floats to be read.
+    """
+    return _read(stm, '>{}f'.format(count))
+
+def read_utf_string(stm, count = 1):
+    """Read UTF-8 encoded string(s) from stream.
+
+    NOTE: Java modified UTF-8 encoding is not supported.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of strings to be read.
+    """
+    ss = []
+    for i in range(count):
+        (size,) = read_short(stm)
+        bs = stm.read(size)
+        ss.append(bs.decode('utf-8'))
+    return tuple(ss)
+
+def read_raw_string(stm, count = 1):
+    """Read raw string(s) from stream.
+
+    Arguments:
+    stm   -- Input stream.
+    count -- Number of strings to be read.
+    """
+    ss = []
+    for i in range(count):
+        (size,) = read_short(stm)
+        bs = stm.read(size)
+        ss.append(bs)
+    return tuple(ss)
+
+def write_byte(stm, *args):
+    """Write byte(s) to stream.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Bytes to be written.
+    """
+    _write(stm, '>{}B'.format(len(args)), *args)
+
+def write_short(stm, *args):
+    """Write half word(s) to stream.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Half words to be written.
+    """
+    _write(stm, '>{}H'.format(len(args)), *args)
+
+def write_int(stm, *args):
+    """Write integer(s) to stream.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Integers to be written.
+    """
+    _write(stm, '>{}I'.format(len(args)), *args)
+
+def write_float(stm, *args):
+    """Write float(s) to stream.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Floats to be written.
+    """
+    _write(stm, '>{}f'.format(len(args)), *args)
+
+def write_utf_string(stm, *args):
+    """Write UTF-8 encoded string(s) to stream.
+
+    NOTE: Java modified UTF-8 encoding is not supported.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Strings to be written.
+    """
+    for s in args:
+        bs = s.encode('utf-8')
+        write_short(stm, len(bs))
+        stm.write(bs)
+
+def write_raw_string(stm, *args):
+    """Write raw string(s) to stream.
+
+    Arguments:
+    stm   -- Output stream.
+    *args -- Strings to be written.
+    """
+    for bs in args:
+        write_short(stm, len(bs))
+        stm.write(bs)
+
+def _read(stm, fmt):
+    bs = stm.read(struct.calcsize(fmt))
+    return struct.unpack_from(fmt, bs, 0)
+
+def _write(stm, fmt, *args):
+    bs = bytearray(struct.calcsize(fmt))
+    struct.pack_into(fmt, bs, 0, *args)
+    stm.write(bs)
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,9 @@
 import unittest
 import random
+import io
 import kvenjoy.tea
 import kvenjoy.cipher
+import kvenjoy.io
 
 class TestTEA(unittest.TestCase):
     plain = bytes([2, 0, 0, 9, 0, 2, 1, 6])
@@ -26,6 +28,73 @@ class TestCipher(unittest.TestCase):
             p = kvenjoy.cipher.decrypt(c, TestCipher.key)
             self.assertEqual(','.join([' {:02x}'.format(x) for x in p]),
                              ','.join([' {:02x}'.format(x) for x in plain]))
+
+class TestIO(unittest.TestCase):
+    def test_read_byte(self):
+        stm = io.BytesIO(bytes(range(16)))
+        bs = kvenjoy.io.read_byte(stm, 16)
+        self.assertEqual(bs, (0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f))
+
+    def test_write_byte(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_byte(stm, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f)
+        stm.seek(0)
+        self.assertEqual(stm.read(), bytes(range(16)))
+
+    def test_read_short(self):
+        stm = io.BytesIO(bytes(range(16)))
+        shorts = kvenjoy.io.read_short(stm, 8)
+        self.assertEqual(shorts, (0x0001, 0x0203, 0x0405, 0x0607, 0x0809, 0x0a0b, 0x0c0d, 0x0e0f))
+
+    def test_write_short(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_short(stm, 0x0001, 0x0203, 0x0405, 0x0607, 0x0809, 0x0a0b, 0x0c0d, 0x0e0f)
+        stm.seek(0)
+        self.assertEqual(stm.read(), bytes(range(16)))
+
+    def test_read_int(self):
+        stm = io.BytesIO(bytes(range(16)))
+        ints = kvenjoy.io.read_int(stm, 4)
+        self.assertEqual(ints, (0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f))
+
+    def test_write_int(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_int(stm, 0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f)
+        stm.seek(0)
+        self.assertEqual(stm.read(), bytes(range(16)))
+
+    def test_read_float(self):
+        stm = io.BytesIO(bytes([0x44, 0xf7, 0x40, 0x00, 0x44, 0xf7, 0xa0, 0x00, 0x44, 0xf9, 0xe0, 0x00, 0x44, 0xfa, 0x20, 0x00]))
+        floats = kvenjoy.io.read_float(stm, 4)
+        self.assertEqual(floats, (1978, 1981, 1999, 2001))
+
+    def test_write_float(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_float(stm, 1978, 1981, 1999, 2001)
+        stm.seek(0)
+        self.assertEqual(stm.read(),bytes([0x44, 0xf7, 0x40, 0x00, 0x44, 0xf7, 0xa0, 0x00, 0x44, 0xf9, 0xe0, 0x00, 0x44, 0xfa, 0x20, 0x00]))
+
+    def test_read_utf_string(self):
+        stm = io.BytesIO(bytes([0x00, 0x03, 0x59, 0x49, 0x4e, 0x00, 0x04, 0x59, 0x41, 0x4e, 0x47]))
+        strings = kvenjoy.io.read_utf_string(stm, 2)
+        self.assertEqual(strings, ('YIN', 'YANG'))
+
+    def test_write_utf_string(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_utf_string(stm, 'YIN', 'YANG')
+        stm.seek(0)
+        self.assertEqual(stm.read(), bytes([0x00, 0x03, 0x59, 0x49, 0x4e, 0x00, 0x04, 0x59, 0x41, 0x4e, 0x47]))
+
+    def test_read_raw_string(self):
+        stm = io.BytesIO(bytes([0x00, 0x03, 0x59, 0x49, 0x4e, 0x00, 0x04, 0xed, 0x41, 0x4e, 0x47]))
+        strings = kvenjoy.io.read_raw_string(stm, 2)
+        self.assertEqual(strings, (b'YIN', b'\xedANG'))
+
+    def test_write_raw_string(self):
+        stm = io.BytesIO(bytearray())
+        kvenjoy.io.write_raw_string(stm, b'YIN', b'\xedANG')
+        stm.seek(0)
+        self.assertEqual(stm.read(), bytes([0x00, 0x03, 0x59, 0x49, 0x4e, 0x00, 0x04, 0xed, 0x41, 0x4e, 0x47]))
 
 if __name__ == '__main__':
     unittest.main(),
