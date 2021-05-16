65a365968b3678eccbcefab061fcfc22f734e1f3
Ying YIN <ying.yin.buzz@outlook.com>
2021-05-16 19:37:04 +0800

    Add GUI property/event binding test code.

--- gfontmaker.py
+++ gfontmaker.py
@@ -6,6 +6,8 @@ from kivy.properties import NumericProperty
 class GfontMaker(App):
     threshold = NumericProperty(128)
 
+    def on_threshold(self, *args):
+        print('threshold changed to {}'.format(self.threshold))
+
 if __name__ == '__main__':
-    app = GfontMaker()
-    app.run()
+    GfontMaker().run()
