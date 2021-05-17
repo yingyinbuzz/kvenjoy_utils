8d0fce8f3f8d518a836f6fda041acc6cb9956209
Ying YIN <ying.yin.buzz@outlook.com>
2021-05-17 14:13:29 +0800

    Add image file load dialog.

--- gfontmaker.py
+++ gfontmaker.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
 
+import os
 from kivy.app import App
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 class RootWidget(BoxLayout):
     threshold = NumericProperty(128)
+    last_path = StringProperty(os.getcwd())
 
     def on_threshold(self, *args):
         print('threshold changed to {}'.format(self.threshold))
 
+    def load(self, path, filenames):
+        print('Load {} {}'.format(path, filenames))
+        self.last_path = path
+        self.ids.img.source = filenames[0]
+        self.dismiss_popup()
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
     def on_image_clicked(self, instance, touch):
         if touch.is_double_tap:
-            print('double clicked')
+            content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+            content.ids.file_chooser.path = self.last_path
+            self._popup = Popup(title='Load file', content=content)
+            self._popup.open()
 
 class GfontMakerApp(App):
     pass
