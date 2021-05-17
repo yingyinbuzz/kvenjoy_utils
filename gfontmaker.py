92874850f53ecf0c8972cb67767e47486f299fd6
Ying YIN <ying.yin.buzz@outlook.com>
2021-05-17 13:38:22 +0800

    Hook double-tap event on image widget.

--- gfontmaker.py
+++ gfontmaker.py
@@ -2,12 +2,20 @@
 
 from kivy.app import App
 from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
 
-class GfontMaker(App):
+class RootWidget(BoxLayout):
     threshold = NumericProperty(128)
 
     def on_threshold(self, *args):
         print('threshold changed to {}'.format(self.threshold))
 
+    def on_image_clicked(self, instance, touch):
+        if touch.is_double_tap:
+            print('double clicked')
+
+class GfontMakerApp(App):
+    pass
+
 if __name__ == '__main__':
-    GfontMaker().run()
+    GfontMakerApp().run()
