f2d0245941f446272e3c9da9f7840c2ff6920629
Ying YIN <ying.yin.buzz@outlook.com>
2021-05-22 10:38:45 +0800

    Find red and green horizontal lines in an image.

--- gfontmaker.py
+++ gfontmaker.py
@@ -7,11 +7,11 @@ from functools import partial
 from PIL import Image
 from kivy.core.image import Image as CoreImage
 from kivy.app import App
-from kivy.properties import NumericProperty, ObjectProperty, StringProperty
+from kivy.properties import NumericProperty, ObjectProperty, StringProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-from kivy.graphics import Color, Rectangle, Translate, Scale
+from kivy.graphics import Color, Rectangle, Translate, Scale, Line
 from kivy.clock import mainthread
 
 class LoadDialog(FloatLayout):
@@ -20,17 +20,30 @@ class LoadDialog(FloatLayout):
 
 class RootWidget(BoxLayout):
     threshold = NumericProperty(128)
+    stroke_threshold = NumericProperty(16)
     last_path = StringProperty(os.getcwd())
     image_orig = ObjectProperty(None)
+    green_lines = ListProperty()
+    red_lines = ListProperty()
 
     @mainthread
     def update_image(self, data, *args):
         self.ids.img.texture = CoreImage(data, ext='jpeg').texture
-        self.draw_block()
+
+        img = self.ids.img
+        img.canvas.after.clear()
+        with img.canvas.after:
+            Translate(0, img.texture_size[1])
+            Scale(1, -1, 0)
+            Color(0, 1, 0)
+            for line in self.green_lines:
+                Line(points=line)
+            Color(1, 0, 0)
+            for line in self.red_lines:
+                Line(points=line)
 
     def on_threshold(self, *args):
         print('threshold changed to {}'.format(self.threshold))
-        self.draw_block()
 
     def on_load_image_file(self, path, filenames):
         self.last_path = path
@@ -49,21 +62,36 @@ class RootWidget(BoxLayout):
         self.image_orig = Image.open(filename)
         data = io.BytesIO()
         self.image_orig.save(data, format='jpeg')
+        base_lines = self.find_base_lines(self.image_orig, self.stroke_threshold)
         data.seek(0)
         self.update_image(data)
 
-    def draw_block(self):
-        img = self.ids.img
-        height = img.texture_size[1]
-        x = self.threshold * 5
-        y = self.threshold * 5
-        box_size = 50
-        img.canvas.after.clear()
-        with img.canvas.after:
-            Color(1, 0, 0)
-            Translate(0, height)
-            Scale(1, -1, 0)
-            Rectangle(pos=(x, y), size=(box_size, box_size))
+    def add_to_y(self, ys, y, threshold):
+        for i in range(len(ys)):
+            p = ys[i]
+            if abs(p[0] - y) <= threshold:
+                ys[i] = (p[0], p[1] + 1)
+                return
+        ys.append((y, 1))
+
+    def find_base_lines(self, image, stroke_threshold):
+        w, h = image.size
+        green_ys = []
+        red_ys = []
+        color_threshold = 25
+        for y in range(h):
+            for x in range(w):
+                r, g, b = image.getpixel((x, y))
+                if r - g >= color_threshold and r - b >= color_threshold:
+                    self.add_to_y(red_ys, y, stroke_threshold)
+                elif g - r >= color_threshold and g - b >= color_threshold:
+                    self.add_to_y(green_ys, y, stroke_threshold)
+        self.green_lines.clear()
+        for (y, c) in green_ys:
+            self.green_lines.append([0, y, w - 1, y])
+        self.red_lines.clear()
+        for (y, c) in red_ys:
+            self.red_lines.append([0, y, w - 1, y])
 
 class GfontMakerApp(App):
     pass
