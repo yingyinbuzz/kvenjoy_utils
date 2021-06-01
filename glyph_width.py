8b2d8c3f224b775532b1b69123848348a61b0360
Ying YIN <ying.yin.buzz@outlook.com>
2021-06-01 13:29:02 +0800

    Add diagonal scans to stroke width detection algorithm.

--- glyph_width.py
+++ glyph_width.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import PIL.Image
+from PIL import Image
+from math import sqrt
 
 class IntervalTracker:
     def __init__(self):
@@ -28,7 +29,7 @@ def is_stroke(p):
 
 for fn in sys.argv[1:]:
     print(fn)
-    image = PIL.Image.open(fn)
+    image = Image.open(fn)
     w, h = image.size
     print('Size {}x{}'.format(w, h))
 
@@ -44,6 +45,35 @@ for fn in sys.argv[1:]:
             it.track_at(y, is_stroke(image.getpixel((x, y))))
         it.track_at(h, False)
 
+    if w > h:
+        for x in range(w + h):
+            sx = min(x, w - 1)
+            for y in range(max(0, x - w), min(h, x)):
+                it.track_at(round(y * sqrt(2)), is_stroke(image.getpixel((sx, y))))
+                sx -= 1
+            it.track_at(round(min(h, x) * sqrt(2)), False)
+    else:
+        for y in range(h + w):
+            sy = min(y, h - 1)
+            for x in range(max(0, y - h), min(w, y)):
+                it.track_at(round(x * sqrt(2)), is_stroke(image.getpixel((x, sy))))
+                sy -= 1
+            it.track_at(round(min(w, y) * sqrt(2)), False)
+
+    if w > h:
+        for x in range(-h, w):
+            sx = max(0, x)
+            for y in range(max(0, -x), min(h, w - x)):
+                it.track_at(round(y * sqrt(2)), is_stroke(image.getpixel((sx, y))))
+                sx += 1
+            it.track_at(round(min(h, w - x) * sqrt(2)), False)
+    else:
+        for y in range(-w, h):
+            sy = max(0, y)
+            for x in range(max(0, -y), min(w, h - y)):
+                it.track_at(round(x * sqrt(2)), is_stroke(image.getpixel((x, sy))))
+                sy += 1
+            it.track_at(round(min(w, h - y) * sqrt(2)), False)
 
     for interval, count in sorted(((k, it.intervals[k]) for k in it.intervals), reverse=True, key=lambda p : p[1]):
         print(' {}:{}'.format(interval, count), end='')
