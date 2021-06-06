fcb6b05e361088ca47c98a507f3104528b20bb30
Ying YIN <ying.yin.buzz@outlook.com>
2021-06-06 19:14:38 +0800

    Make stroke width detector handle RGB images.

--- glyph_width.py
+++ glyph_width.py
@@ -4,11 +4,15 @@ import sys
 from PIL import Image
 from tracer.stroke_size import *
 
-def is_stroke(p):
+def is_stroke_grayscale(p):
     return p < 128
+
+def is_stroke_rgb(p):
+    r, g, b = p
+    return r < 128 and g < 128 and b < 128
 
 for fn in sys.argv[1:]:
     print(fn)
     image = Image.open(fn)
-    print('Stroke size = {}'.format(scan_stroke_size(image, is_stroke)))
+    print('Stroke size = {}'.format(scan_stroke_size(image, is_stroke_rgb if image.mode == 'RGB' else is_stroke_grayscale)))
 
